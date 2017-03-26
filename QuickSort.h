274f4cbafae26573253dad77a5d2cf665b5773c3
OneE <774247851@qq.com>
2017-03-26 14:52:42 +0800

    完成三路快速排序

--- QuickSort.h
+++ QuickSort.h
@@ -79,8 +79,44 @@ void __quickSort(T arr[], int l, int r) {
     __quickSort(arr, p + 1, r);
 }
 
+// 三路快速排序处理 arr[l...r]
+// 将arr[l...r]分为 <v ; ==v ; >v 三部分
+// 之后对 <v 和 >v 的部分继续进行三路快速排序
+template <typename T>
+void __quickSort3Ways(T arr[], int l, int r) {
+
+    if (r - l <= 15) {
+        insertionSort(arr, l, r);
+        return;
+    }
+
+    // partition
+    swap(arr[l], arr[rand()%(r-l+1)+l]);
+    T v = arr[l];
+
+    int lt = l;     // arr[l+1...lt] < v
+    int gt = r + 1; // arr[gt...r] > v
+    int i = l + 1;  // arr[lt+1...i] == v
+    while (i < gt) {
+        if (arr[i] < v) {
+            swap(arr[i], arr[lt+1]);
+            lt ++;
+            i ++;
+        } else if (arr[i] > v) {
+            swap(arr[i], arr[gt-1]);
+            gt --;
+        } else {
+            i ++;
+        }
+    }
+    swap(arr[l], arr[lt]);
+
+    __quickSort3Ways(arr, l, lt-1);
+    __quickSort3Ways(arr, gt, r);
+}
+
 /**
- * 快速排序
+ * 普通快速排序
  * @tparam T
  * @param arr
  * @param n
@@ -90,6 +126,19 @@ void quickSort(T arr[], int n) {
 
     srand(time(NULL));
     __quickSort(arr, 0, n - 1);
+}
+
+/**
+ * 三路快速排序
+ * @tparam T
+ * @param arr
+ * @param n
+ */
+template <typename T>
+void quickSort3Ways(T arr[], int n) {
+
+    srand(time(NULL));
+    __quickSort3Ways(arr, 0, n-1);
 }
 
 #endif //ALGORITHMS_QUICKSORT_H
--- main.cpp
+++ main.cpp
@@ -11,21 +11,45 @@ int main() {
 
     int n = 1000000;
 
+    // 一般性排序测试
     cout<<"Test for Random Array, size = "<<n<<", random range [0, "<<n<<"]"<<endl;
     int *arr1 = SortTestHelper::generateRandomArray(n, 0, n);
     int *arr2 = SortTestHelper::copyIntArray(arr1, n);
+    int *arr3 = SortTestHelper::copyIntArray(arr1, n);
     SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
     SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
+    SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr3, n);
+    delete(arr1);
+    delete(arr2);
+    delete(arr3);
+    cout<<endl;
 
+    // 几乎有序的数组排序测试
     int swapTimes = 100;
     cout<<"Test for Random Nearly Ordered Array, size = "<<n<<", swap time = "<<swapTimes<<endl;
     arr1 = SortTestHelper::generateNearlyOrderedArray(n, swapTimes);
     arr2 = SortTestHelper::copyIntArray(arr1, n);
+    arr3 = SortTestHelper::copyIntArray(arr1, n);
     SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
     SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
-
+    SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr3, n);
     delete(arr1);
     delete(arr2);
+    delete(arr3);
+    cout<<endl;
+
+    // 包含大量重复元素的数组排序测试
+    cout<<"Test for Random Array, size = "<<n<<", random range [0, 10]"<<endl;
+    arr1 = SortTestHelper::generateRandomArray(n, 0, 10);
+    arr2 = SortTestHelper::copyIntArray(arr1, n);
+    arr3 = SortTestHelper::copyIntArray(arr1, n);
+    SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
+    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
+    SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr3, n);
+    delete(arr1);
+    delete(arr2);
+    delete(arr3);
+    cout<<endl;
 
     return 0;
 }
